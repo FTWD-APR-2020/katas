e24a8e1f01cb2d0ff138e1af5d4591d5ac9f97ee
cule219 <culaficstefan@gmail.com>
2020-04-14 10:14:54 -0400

    christmass-tree

--- 6kyu.js
+++ 6kyu.js
@@ -5,3 +5,28 @@ var uniqueInOrder = function (iterable) {
   //your code here - remember iterable can be a string or an array
   return [...iterable].filter((item, index, arr) => item !== arr[index + 1]);
 };
+
+function christmasTree(height) {
+  //   let xmasT = '';
+  //   for(let i = 0; i<height; i++) {
+  //     for(let j = 0; j < (height -i -1); j++) {
+  //       xmasT+= " ";
+  //     }
+  //     for(let k = 0; k < (2*i+1); k++) {
+  //       xmasT+="*";
+  //     }
+  //     for(let r = 0; r < (height -i -1); r++) {
+  //       xmasT+= " ";
+  //     }
+  //     if(i !== height-1) {
+  //       xmasT += '\n';
+  //     }
+  //   }
+  //   return xmasT;
+  return [...new Array(height).keys()]
+    .map((row, i) => row + i + 1)
+    .reverse()
+    .map((row, i) => " ".repeat(i) + "*".repeat(row) + " ".repeat(i))
+    .reverse()
+    .join("\n");
+}
