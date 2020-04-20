b98c969a9567e822b0318744735adea3cb0d4dcf
Cule219 <culaficstefan@gmail.com>
2020-04-20 14:33:47 -0400

    onlyduplicates

--- 6kyu.js
+++ 6kyu.js
@@ -30,3 +30,16 @@ function christmasTree(height) {
     .reverse()
     .join("\n");
 }
+
+// https://www.codewars.com/kata/5a1dc4baffe75f270200006b
+function onlyDuplicates(str) {
+  // your code here
+
+  aaaccccabc; // 0 8 - a
+  //  0 !== 8 -> true
+  // 9 !== 9 -> false
+  return str
+    .split("")
+    .filter((e) => str.indexOf(e) !== str.lastIndexOf(e))
+    .join("");
+}
