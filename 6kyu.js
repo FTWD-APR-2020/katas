4c1337b407af4651f847232808f961f95e5bd089
Cule219 <culaficstefan@gmail.com>
2020-05-07 10:18:56 -0400

    more-zeroes-than-ones

--- 6kyu.js
+++ 6kyu.js
@@ -134,3 +134,36 @@ function isPangram(string) {
     (char) => string.toLowerCase().indexOf(char) !== -1
   );
 }
+
+// https://www.codewars.com/kata/5d41e16d8bad42002208fe1a/train/javascript
+function moreZeros(s) {
+  let array = s.split("");
+  let newArray = array.map((char) => {
+    return char.charCodeAt(0).toString(2);
+  });
+  let solution = [];
+  for (let i = 0; i < newArray.length; i++) {
+    let zeroCount = 0;
+    let oneCount = 0;
+    for (let j = 0; j < newArray[i].length; j++) {
+      if (newArray[i][j] == 0) {
+        zeroCount++;
+      } else {
+        oneCount++;
+      }
+    }
+    if (oneCount < zeroCount) {
+      solution.push(s[i]);
+    }
+  }
+  return [...new Set(solution)];
+}
+
+// Solution 2
+
+// return [...new Set([...s].filter((letter) => {
+//     let binary = letter.charCodeAt().toString(2);
+//     let numberOfZeroes = binary.replace(/1/g,'').length;  //.filter(num => num*1 === 0).length;
+//     let numberOfOnes = binary.replace(/0/g,'').length;  //.filter(num => num*1 === 1).length;
+//     return numberOfZeroes > numberOfOnes;
+//   }))]
